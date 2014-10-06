2c5f1ff2488e5441742acf35451630b5e4bcf6bc
vitolpvv <admin@Mac-Admin.local>
2014-10-07 01:03:19 +0400

    Initial Commit

---
+++ iFlickr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iFlickr
+//
+//  Created by VitaliyP on 07.10.14.
+//  Copyright (c) 2014 VitaliyP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iFlickr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iFlickr
+//
+//  Created by VitaliyP on 07.10.14.
+//  Copyright (c) 2014 VitaliyP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
